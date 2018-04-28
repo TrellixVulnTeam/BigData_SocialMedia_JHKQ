c6fd5ca212a6e785e9533a2c935cbd83832a8d28
User <musamthembu84@gmail.com>
2018-04-28 22:08:55 +0200

    using regex expressions for spilts

--- App/textProcess.py
+++ App/textProcess.py
@@ -1,5 +1,6 @@
 import json
 from nltk.tokenize import word_tokenize
+import re 
 
 data_store='C:/Users/tammy/Documents/python/data.json'
 
@@ -10,3 +11,20 @@ with open (data_store, 'r') as f:
 
 each_tweet= 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
 print(word_tokenize(each_tweet))
+
+
+emoticons_str = r 
+
+regex_str = [
+           emoticons_str,
+           r'<[^>]+>', # HTML tags
+           r'(?:@[\w_]+)', # @-mentions
+           r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+           r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+           r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+           r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+           r'(?:[\w_]+)', # other words
+           r'(?:\S)' # anything else
+          
+]
