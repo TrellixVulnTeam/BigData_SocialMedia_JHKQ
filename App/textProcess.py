7d14c1daa62e250e68006426e43136f42d96047d
User <musamthembu84@gmail.com>
2018-05-14 12:54:45 +0200

    Extracting emoticons from json file

--- App/textProcess.py
+++ App/textProcess.py
@@ -1,30 +1,45 @@
+import sys
+import os
+import re
 import json
-from nltk.tokenize import word_tokenize
-import re 
-
-data_store='C:/Users/tammy/Documents/python/data.json'
-
-with open (data_store, 'r') as f:
-    line = f.readline()
-    tweet=json.loads(line)
-   # print(json.dumps(tweet, indent=4))
-
-each_tweet= 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
-print(word_tokenize(each_tweet))
 
 
-emoticons_str = r 
-
-regex_str = [
-           emoticons_str,
-           r'<[^>]+>', # HTML tags
-           r'(?:@[\w_]+)', # @-mentions
-           r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
-           r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
- 
-           r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
-           r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
-           r'(?:[\w_]+)', # other words
-           r'(?:\S)' # anything else
-          
+#emoticons extracted 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )""" 
+regex_str  = [
+    emoticons_str,
+    r'<[^>]+>',#HTML hash tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
 ]
+
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+ 
+tweet=''
+with open ('text.json' , 'r') as f:
+   # line = f.readline()
+   for line in f:
+    tweet=json.loads(line)
+    tokens = preprocess(tweet['text'])
+print (tokens)
+
