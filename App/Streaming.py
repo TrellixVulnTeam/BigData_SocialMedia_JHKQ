f96b41d3c7b9a33d1473acfb2e30e2f6452c7bfd
User <musamthembu84@gmail.com>
2018-07-10 17:43:10 +0200

    Updates on file

--- App/Streaming.py
+++ App/Streaming.py
@@ -4,13 +4,18 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import tweepy
 import os.path 
+from TwitterWall import Tweets
 
-ckey= 'ziEH3cxbutb6Yspn80pgBjRgD'
-csecret= 'Wm3gCR4RA7Mx0cLl8WuwfkAZJxobBep0yVSuw492OQ1PEoakJE'
-atoken= '703575776936005632-3smypE0iZTFxNXO6pEq1bM5rxwYBQix'
-asecret= 'vpQrjsZZRsNRTzGYF0RhFKpBETQwP6qJeCo1cG8gBimfX'
+
 KeyWords=['#worldcup ','#fifaworldcup','#wc2018','#2018worldcup','#russia2018']
 data_store='C:/Users/tammy/Documents/python/final.json'
+
+Tweets = Tweets( 'ziEH3cxbutb6Yspn80pgBjRgD', 'Wm3gCR4RA7Mx0cLl8WuwfkAZJxobBep0yVSuw492OQ1PEoakJE',
+                 '703575776936005632-3smypE0iZTFxNXO6pEq1bM5rxwYBQix','vpQrjsZZRsNRTzGYF0RhFKpBETQwP6qJeCo1cG8gBimfX')
+ckey = Tweets.CKey
+csecret = Tweets.CSecret
+atoken = Tweets.AToken
+asecret = Tweets.ASToken
 class listener(StreamListener):
 
     def on_data(self,data):
