293bb6d7aa3449e045cb223f9042ef5b5459af62
User <musamthembu84@gmail.com>
2018-07-10 14:00:04 +0200

    class responsible for all keys and access tokens

---
+++ App/TwitterWall.py
@@ -0,0 +1,35 @@
+
+class Tweets():
+
+    def __init__(self):
+         self.CKey = 'ziEH3cxbutb6Yspn80pgBjRgD'
+         self.CSecret = 'Wm3gCR4RA7Mx0cLl8WuwfkAZJxobBep0yVSuw492OQ1PEoakJE'
+         self.AToken = '703575776936005632-3smypE0iZTFxNXO6pEq1bM5rxwYBQix'
+         self.ASToken = 'vpQrjsZZRsNRTzGYF0RhFKpBETQwP6qJeCo1cG8gBimfX'
+
+         
+
+    @property
+    def CKey(self):
+         return self.CKey
+    
+    @property
+    def CSecret(self):
+         return self.CKey
+
+    
+    @property
+    def AToken(self):
+         return self.CKey
+
+    
+    @property
+    def ASToken(self):
+         return self.CKey
+
+
+    
+
+
+
+     
