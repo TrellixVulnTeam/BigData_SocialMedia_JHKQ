4c4330dbf90ca52952334d39f6046ade32145711
User <musamthembu84@gmail.com>
2018-07-10 18:40:07 +0200

    TDF class to count terms after removing stopwords

--- App/TDF.py
+++ App/TDF.py
@@ -1,20 +1,36 @@
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 
-sentenceData = spark.createDataFrame([
-    (0.0, "Hi I heard about Spark"),
-    (0.0, "I wish Java could use case classes"),
-    (1.0, "Logistic regression models are neat")
-], ["label", "sentence"])
 
-tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
-wordsData = tokenizer.transform(sentenceData)
+class TDF():
+    def __init__(self, FileName):
+         self.name = FileName
 
-hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
-featurizedData = hashingTF.transform(wordsData)
-# alternatively, CountVectorizer can also be used to get term frequency vectors
+    def tokens (self, sentenceData):
+        tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
+        wordsData = tokenizer.transform(sentenceData)
 
-idf = IDF(inputCol="rawFeatures", outputCol="features")
-idfModel = idf.fit(featurizedData)
-rescaledData = idfModel.transform(featurizedData)
+    def hashing(self, wordsData):
 
-rescaledData.select("label", "features").show()
+        hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
+        featurizedData = hashingTF.transform(wordsData)
+       # alternatively, CountVectorizer can also be used to get term frequency vectors
+
+        idf = IDF(inputCol="rawFeatures", outputCol="features")
+        idfModel = idf.fit(featurizedData)
+        rescaledData = idfModel.transform(featurizedData)
+
+        rescaledData.select("label", "features").show()
+    
+
+    def stopWords():
+        myfile =open("C:/Users/tammy/Documents/python/final.txt")
+        stop_words = set(stopwords.words('english'))
+        line = myfile.read()
+        words = line.split()
+    for r in words:
+        if not r in stop_words:
+           appendfile = open('C:/Users/tammy/Documents/python/clean.txt', 'a')
+           appendfile.write(" "+r)
+           appendfile.close()
+
+    print ("success")
