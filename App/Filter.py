699c1747602a6a8763c666da8d627862565cb123
User <musamthembu84@gmail.com>
2018-07-10 17:16:44 +0200

    regex expression to validate text

--- App/Filter.py
+++ App/Filter.py
@@ -6,7 +6,23 @@ class Filter():
     def __init__(self, FileName):
         self.name = FileName
 
-    def RegFormula():
-        
+    def RegFormula(self):
+         emoticons_str = r"""
+        (?:
+           [:=;] # Eyes
+           [oO\-]? # Nose (optional)
+           [D\)\]\(\]/\\OpP] # Mouth
+        )""" 
+         regex_str  = [
+         emoticons_str,
+         r'<[^>]+>',#HTML hash tags
+         r'(?:@[\w_]+)', # @-mentions
+         r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+         r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+         r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+         r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+         r'(?:[\w_]+)', # other words
+         r'(?:\S)' # anything else
+        ]
 
     
