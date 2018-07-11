5344418ebbf168b3e605d2f6324186b6cd36fee5
User <musamthembu84@gmail.com>
2018-07-11 12:51:11 +0200

    adding visualisation function

--- App/TDF.py
+++ App/TDF.py
@@ -4,6 +4,7 @@ from collections import Counter
 from nltk import word_tokenize
 from nltk.tokenize import wordpunct_tokenize
 import sys
+import matplotlib.pyplot as plt 
 
 class TDF():
     def __init__(self, FileName, ):
@@ -41,15 +42,40 @@ class TDF():
     print ("success")
 
     def doCounter(self):
-        myfile =open("C:/Users/tammy/Documents/python/Ready.txt", "r", encoding="utf-8-sig")
+        myfile =open("C:/Users/tammy/Documents/python/clean.txt", "r", encoding="utf-8-sig")
         wordcount = Counter(myfile.read().split()) 
-        for item in wordcount.items():
-            print ("{}\t{}".format(*item))
-            print(wordcount.most_common(20))
+        #for item in wordcount.items():
+            #print ("{}\t{}".format(*item))
+        dic =  wordcount.most_common(20)
+        return dic 
 
+    def Visual(self):
+        plt.subplot(111, facecolor='w')
+        alignment = {'horizontalalignment': 'center', 'verticalalignment': 'baseline'}
+
+              
+        families = []
+        myf =  open ("C:/Users/tammy/Documents/python/wow.txt")
+        line = myf.read()
+        words = line.split()
+        for w in words:
+            families.append(w)
+
+        wordcount = Counter(families)
+        print(wordcount.most_common(6))
+        t = plt.text(0.0, 0.9, 'Hot Topics World Cup', size='large', **alignment)
+
+        yp = [0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]
+
+        for k, family in enumerate(wordcount.most_common(6)):
+            t = plt.text(0.0, yp[k], family, family=family, **alignment)
+            x = -0.4
+
+        plt.axis([-1, 1, 0, 1])
+        plt.show()
 
 if __name__ == "__main__":
     original = sys.argv[1]
     myTdf = TDF(original)
-    myTdf.stopWordsRemove()
-    myTdf.doCounter()
+    #myTdf.stopWordsRemove()
+    print (myTdf.doCounter())
