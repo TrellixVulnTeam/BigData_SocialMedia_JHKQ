d09137b626094ffa87c6d2bd642f49942bf6883e
User <musamthembu84@gmail.com>
2018-07-24 09:18:37 +0200

    run test cases

--- App/run.py
+++ App/run.py
@@ -1,81 +1,59 @@
-
-import json
-import matplotlib.pyplot as plt
-import pandas as pd   
-import re, string  
-from nltk.corpus import stopwords
-from collections import Counter
-from nltk import word_tokenize
-from nltk.tokenize import wordpunct_tokenize
-import ijson 
-from itertools import islice
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+import pyLDAvis.gensim
+import sys
 
 
+class LDA:
+    def __init__(self, oldFile):
+        self.oldfile = oldFile
+    def do(self):
+        tokenizer = RegexpTokenizer(r'\w+')
 
-fname = 'C:/Users/tammy/Documents/python/data.json'
-tweet_data = []
-tweet_file = open (fname, "r")
+# create English stop words list
+        en_stop = get_stop_words('en')
 
+        p_stemmer = PorterStemmer()
 
-def read():    
-    for line in tweet_file:
-        try:
-            tweet= json.loads(line)
-            tweet_data.append(tweet)
-        except:
-         continue
-    
-    return tweet_data         
+        tweet_file = open(self.oldfile, "r")
+# create sample documents
 
-def strip_links(text):
-    return  ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
-
-def writeToDB():
-    myfile = "C:/Users/tammy/Documents/python/final.txt"
-    tweet = read()
-    with open (myfile, 'w') as f:
+        doc = []
+        for w in tweet_file:
+            doc.append(w)
+        doc_set = doc 
+        texts = []
         
-        for t in tweet:
-            data = strip_links(t['text'])
-            try:
-               f.write(data)
-            except:
-                continue
-        #f.close()
-            
-      
+        for i in doc_set:
+            raw = i.lower()
+        tokens = tokenizer.tokenize(raw)
 
-def display():
-     myfile =open("C:/Users/tammy/Documents/python/clean.txt", "r", encoding="utf-8-sig")
-     wordcount = Counter(myfile.read().split())
-    
-     
-     for item in wordcount.items():
-        print ("{}\t{}".format(*item))
-     
-     print(wordcount.most_common(10))      
+        stopped_token = [i for i in tokens if not i in en_stop]
 
+        stemmed_tokens = [p_stemmer.stem(i) for i in stopped_token]
 
-def trial():
-    myfile =open("C:/Users/tammy/Documents/python/final.txt")
-    stop_words = set(stopwords.words('english'))
-    line = myfile.read()
-    words = line.split()
-    for r in words:
-        if not r in stop_words:
-           appendfile = open('C:/Users/tammy/Documents/python/clean.txt', 'a')
-           appendfile.write(" "+r)
-           appendfile.close()
+        texts.append(stemmed_tokens)
 
-    print ("success")
-    
+        dictionary = corpora.Dictionary(texts)
+        corpus = [dictionary.doc2bow(text) for text in texts]
+
+# generate LDA model
+        ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=3, id2word = dictionary, passes=4)
+
+        print(ldamodel.print_topics(num_topics=5))
+
+#pyLDAvis.enable_notebook()
+        visualisation = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
+        pyLDAvis.save_html(visualisation, 'C:/Users/tammy/Documents/python/Low_Visualization.html')
+        print ("Visual Created")
 
 
+if __name__ == '__main__':
 
-#trial()
+    load = LDA(sys.argv[1])
+    load.do()
 
 
-display()
-
-#print(read())
-#writeToDB()
