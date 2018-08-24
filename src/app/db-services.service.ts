41f59fd67193bdb720dfc41eaa45a11aba5bb65e
User <musamthembu84@gmail.com>
2018-08-24 10:31:30 +0200

    service methods for POST and GET created

--- src/app/db-services.service.ts
+++ src/app/db-services.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({'content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbServicesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAuthDetails() {
+    const link = '';
+    return this.http.get(link);
+  }
+
+  sendAuthDetails(data) {
+    const packet = [{'Details': data}];
+    const body = JSON.stringify(packet);
+    return this.http.post('', body, httpOptions);
+  }
 }
