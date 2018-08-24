a1f87ee79e07ecd69b61455014e07561cf050183
User <musamthembu84@gmail.com>
2018-08-24 07:11:22 +0200

    routing creating for all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopicsComponent } from './topics/topics.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'index', component : AuthComponent},
+  {path: 'dashboard', component : DashboardComponent},
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: 'topics', component : TopicsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
