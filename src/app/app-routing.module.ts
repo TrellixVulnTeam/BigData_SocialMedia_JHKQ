076696f4accb44320175db351e9fdabfd2b6db47
User <musamthembu84@gmail.com>
2018-08-28 10:49:59 +0200

    routing updated for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TopicsComponent } from './topics/topics.component';
+import { SentimentComponent } from './sentiment/sentiment.component';
+import { GeotweetsComponent } from './geotweets/geotweets.component';
 
 
 const routes: Routes = [
   {path: 'index', component : AuthComponent},
   {path: 'dashboard', component : DashboardComponent},
-  // {path: '', redirectTo: 'index', pathMatch: 'full'},
-  {path: 'topics', component : TopicsComponent}
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: 'topics', component : TopicsComponent},
+  {path: 'sentiment', component : SentimentComponent},
+  {path: 'geotweets', component : GeotweetsComponent}
 ];
 
 @NgModule({
