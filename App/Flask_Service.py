8e0144a7d0f080c7000c50850a500c80a333641b
User <musamthembu84@gmail.com>
2018-09-03 08:14:57 +0200

    updated services

--- App/Flask_Service.py
+++ App/Flask_Service.py
@@ -1,17 +1,22 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api 
-import json
+from flask import Flask,render_template,request,jsonify
+from flask_restful import Resource,Api
 from flask_cors import CORS
+import json
+import sys
 
-app = Flask(__name__) #create the Flask app
+app = Flask(__name__)  #create flask app
 CORS(app)
-
-@app.route('/model', methods=['POST']) #GET requests will be blocked
-def doResponse():
-    response = ""
-    data = request.get_json()
-    print(data)
-    return jsonify(response)
+@app.route('/home')
+def forms():
+    return 0
+@app.route('/model', methods=['POST'])
+def demo():
+    info = request.get_json()
+    aplha = info[0]['alpha']
+    beta = info[0]['beta']
+    print(aplha + " " + beta)
+    concat = aplha + " " + beta     
+    return  jsonify({'text' : concat})
 
 if __name__ == '__main__':
     app.run(debug=True)
